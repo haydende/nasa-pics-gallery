182f74653f43658bd4052644684263fdf87afb05
Hayden Eastwell <haydeneastwell@outlook.com>
2024-04-07 13:12:45 +0100

    WIP - Initial setup of client portion

--- main.js
+++ main.js
@@ -1,24 +1,8 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+const response = await fetch("http://localhost:8080/apod");
 
-setupCounter(document.querySelector('#counter'))
+const { url } = await response.json();
+console.log(url)
+const result = document.querySelector('#result')
+
+result.innerHTML = `<img src="${url}" width="512"/>`
